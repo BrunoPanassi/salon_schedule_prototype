ab575df6cbd07a82954717dcdf8ba53954f123e4
BrunoPanassi <bhenrique95@outlook.com>
2023-03-21 00:29:37 -0300

    create new roleServiceImplTest method

--- src/main/java/com/schedulesalon/prototype/service/RoleService.java
+++ src/main/java/com/schedulesalon/prototype/service/RoleService.java
@@ -4,6 +4,6 @@ import com.schedulesalon.prototype.model.Person;
 import com.schedulesalon.prototype.model.Role;
 
 public interface RoleService {
-    Role saveRole(Role role) throws Exception;
-    Role findRole(String type);
+    Role save(Role role) throws Exception;
+    Role find(String type);
 }
--- src/test/java/com/schedulesalon/prototype/service/RoleServiceImplTest.java
+++ src/test/java/com/schedulesalon/prototype/service/RoleServiceImplTest.java
@@ -1,17 +1,72 @@
 package com.schedulesalon.prototype.service;
 
+import com.schedulesalon.prototype.model.Role;
+import com.schedulesalon.prototype.repo.ClientRepo;
+import com.schedulesalon.prototype.repo.ManagerRepo;
+import com.schedulesalon.prototype.repo.ProfessionalRepo;
+import com.schedulesalon.prototype.repo.RoleRepo;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class RoleServiceImplTest {
+
+    @Mock
+    private ClientRepo clientRepo;
+    @Mock
+    private ManagerRepo managerRepo;
+    @Mock
+    private ProfessionalRepo professionalRepo;
+    @Mock
+    private RoleRepo roleRepo;
+    private RoleServiceImpl roleService;
+    private ManagerService managerService;
+    private ClientService clientService;
+    private ProfessionalService professionalService;
 
     @BeforeEach
     void setUp() {
+        managerService = new ManagerServiceImpl(managerRepo);
+        clientService = new ClientServiceImpl(clientRepo);
+        professionalService = new ProfessionalServiceImpl(professionalRepo);
+        roleService = new RoleServiceImpl(roleRepo, managerService, clientService, professionalService);
     }
 
     @Test
-    void saveRole() {
+    void itShouldSaveRoleProfessional() throws Exception {
+        //given
+        Role professional = new Role(Role.TypeRole.PROFESSIONAL);
+
+        //when
+        Role roleSaved = roleService.save(professional);
+
+        //then
+        ArgumentCaptor<Role> roleArgumentCaptor = ArgumentCaptor.forClass(Role.class);
+        verify(roleRepo).save(roleArgumentCaptor.capture());
+        Role caputredRole = roleArgumentCaptor.getValue();
+        assertThat(caputredRole).isEqualTo(professional);
+    }
+
+    @Test
+    void itNotShouldSaveRoleBecauseAlreadyExists() {
+    }
+
+    @Test
+    void itShouldSaveClientTable() {
+    }
+
+    @Test
+    void itShouldSaveProfessionalTable() {
+    }
+
+    @Test
+    void itShouldSaveManagerTable() {
     }
 }
